fdd2f73b9fa498a722aff1e82573b7144561ea3f
Contra <contra@maricopa.edu>
2015-12-02 21:12:19 -0800

    init

---
+++ test/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var should = require('should');
+var rtc-everywhere = require('../');
+
+describe('FUNCTIONNAME()', function() {
+  it('should TASKNAME', function(done) {
+    should.exist(rtc-everywhere);
+    done();
+  });
+});
