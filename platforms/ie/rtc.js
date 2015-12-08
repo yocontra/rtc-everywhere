bf39d84b7d9be32f9beae8e2be3daf0fef1c58de
Contra <contra@maricopa.edu>
2015-12-08 14:14:24 +0100

    gum for ie

--- platforms/ie/rtc.js
+++ platforms/ie/rtc.js
@@ -1,3 +1,10 @@
 'use strict';
 
-module.exports = require('../unsupported/rtc');
+var temasys = require('../../lib/temasys');
+
+// TODO: mock all rtc objects until async load
+module.exports = function() {
+  temasys(); // start loading ahead of time
+
+  return {};
+};
