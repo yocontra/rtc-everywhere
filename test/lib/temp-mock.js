2998343db1406a08b51f3996dab7b18905656c6e
Contra <contra@maricopa.edu>
2015-12-11 08:17:24 +0100

    more mock stuff

--- test/lib/temp-mock.js
+++ test/lib/temp-mock.js
@@ -34,6 +34,40 @@ describe('temp-mock', function() {
     mock.resolve(o, actual);
   });
 
+  it('should correctly forward a method after resolve', function(done) {
+    var Clazz = mock({
+      methods: ['test']
+    });
+    var o = new Clazz();
+    o.test(4, 5, 6);
+
+    var calls = 0;
+    var actual = function(){
+      Array.prototype.slice.call(arguments).should.eql([]);
+      return {
+        test: function(a, b, c) {
+          ++calls;
+          if (calls === 1) {
+            a.should.equal(4);
+            b.should.equal(5);
+            c.should.equal(6);
+            return;
+          }
+
+          if (calls === 2) {
+            a.should.equal(7);
+            b.should.equal(8);
+            c.should.equal(9);
+          }
+        }
+      };
+    };
+    mock.resolve(o, actual);
+    o.test(7, 8, 9);
+    calls.should.equal(2);
+    done();
+  });
+
   it('should play back a set', function(done) {
     var Clazz = mock({
       properties: ['test']
@@ -46,6 +80,21 @@ describe('temp-mock', function() {
     };
     var inst = mock.resolve(o, actual);
     inst.test.should.equal(123);
+    done();
+  });
+
+  it('should play back two sets', function(done) {
+    var Clazz = mock({
+      properties: ['test']
+    });
+    var o = new Clazz();
+    o.test = 123;
+    o.test = 456;
+    var actual = function(){
+      return {};
+    };
+    var inst = mock.resolve(o, actual);
+    inst.test.should.equal(456);
     done();
   });
 
