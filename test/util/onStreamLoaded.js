87d7f539ce18464806aca8d09961756cf1f4bf0a
Contra <contra@maricopa.edu>
2015-12-16 20:17:36 +0100

    start some video and audio tests

--- test/util/onStreamLoaded.js
+++ test/util/onStreamLoaded.js
@@ -3,18 +3,111 @@
 var should = require('should');
 var onStreamLoaded = require('../../util/onStreamLoaded');
 
+function createMockStream(tracks) {
+  return {
+    getTracks: function(){
+      return tracks.map(function(t){
+        return {
+          kind: t.type,
+          readyState: t.state || 'playing',
+          muted: t.muted == null ? false : t.muted,
+          enabled: t.enabled == null ? true : t.enabled
+        };
+      });
+    }
+  };
+}
+
 describe('onStreamLoaded()', function() {
-  it.skip('should detect stopped video stream', function(done) {
-
+  it('should detect ended video stream', function(done) {
+    var fakeStream = createMockStream([
+      {
+        type: 'video',
+        state: 'ended'
+      }
+    ]);
+    onStreamLoaded({video: true}, fakeStream, function(err, res){
+      should.exist(err);
+      done();
+    });
   });
-  it.skip('should detect stopped audio stream', function(done) {
-
+  it('should detect muted video stream', function(done) {
+    var fakeStream = createMockStream([
+      {
+        type: 'video',
+        muted: true
+      }
+    ]);
+    onStreamLoaded({video: true}, fakeStream, function(err, res){
+      should.exist(err);
+      done();
+    });
   });
-  it.skip('should detect software muted video stream', function(done) {
-
+  it('should detect disabled video stream', function(done) {
+    var fakeStream = createMockStream([
+      {
+        type: 'video',
+        enabled: false
+      }
+    ]);
+    onStreamLoaded({video: true}, fakeStream, function(err, res){
+      should.exist(err);
+      done();
+    });
   });
-  it.skip('should detect software muted audio stream', function(done) {
-
+  it('should detect missing video stream', function(done) {
+    var fakeStream = createMockStream([{
+      type: 'audio'
+    }]);
+    onStreamLoaded({video: true}, fakeStream, function(err, res){
+      should.exist(err);
+      done();
+    });
+  });
+  it('should detect ended audio stream', function(done) {
+    var fakeStream = createMockStream([
+      {
+        type: 'audio',
+        state: 'ended'
+      }
+    ]);
+    onStreamLoaded({audio: true}, fakeStream, function(err, res){
+      should.exist(err);
+      done();
+    });
+  });
+  it('should detect muted audio stream', function(done) {
+    var fakeStream = createMockStream([
+      {
+        type: 'audio',
+        muted: true
+      }
+    ]);
+    onStreamLoaded({audio: true}, fakeStream, function(err, res){
+      should.exist(err);
+      done();
+    });
+  });
+  it('should detect disabled audio stream', function(done) {
+    var fakeStream = createMockStream([
+      {
+        type: 'audio',
+        enabled: false
+      }
+    ]);
+    onStreamLoaded({audio: true}, fakeStream, function(err, res){
+      should.exist(err);
+      done();
+    });
+  });
+  it('should detect missing audio stream', function(done) {
+    var fakeStream = createMockStream([{
+      type: 'video'
+    }]);
+    onStreamLoaded({audio: true}, fakeStream, function(err, res){
+      should.exist(err);
+      done();
+    });
   });
   it.skip('should detect hardware muted video stream', function(done) {
 
--- util/onStreamLoaded/hasValidTrack.js
+++ util/onStreamLoaded/hasValidTrack.js
@@ -1,12 +1,12 @@
 'use strict';
 
-function isTrackEnabled(t){
-  return t.kind === forType && t.enabled &&
+function isTrackEnabled(type, t){
+  return t.kind === type && t.enabled &&
     !t.muted && t.readyState !== 'ended';
 }
 
-function hasValidTrack(stream, forType){
-  return stream.getTracks().some(isTrackEnabled);
+function hasValidTrack(stream, type){
+  return stream.getTracks().some(isTrackEnabled.bind(null, type));
 }
 
 module.exports = hasValidTrack;
