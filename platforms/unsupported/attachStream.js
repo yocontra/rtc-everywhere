80a0e5779b56ba8643688bad959639c0c09b033f
Contra <contra@maricopa.edu>
2015-12-16 11:59:50 +0100

    remove double exports typo

--- platforms/unsupported/attachStream.js
+++ platforms/unsupported/attachStream.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = module.exports = function(){
+module.exports = function(){
   return function(el, stream) {
     throw new Error('No DOM, what are you doing?');
   };
